94be7e806c6cb944804f389bb58c464679c88954
Eliot Alderson <arnavghosh67@gmail.com>
2024-07-10 15:44:44 +0530

    Catalouge filtering

--- routes/catalouge.py
+++ routes/catalouge.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from schemas.product import Product
-from typing import List, Annotated, Optional
+from typing import List, Annotated, Optional, Dict
 from schemas import ProductResponseModel, Product_w_ID
 from schemas.users import User
 from uuid import uuid4
@@ -16,15 +16,37 @@ router = APIRouter(
 
 """Catalouge routes of this API"""
 
+def filter_params(
+    name: Optional[str] = Query(None),
+    tags: Optional[List[str]] = Query(None),
+    min_price: Optional[float] = Query(None),
+    max_price: Optional[float] = Query(None),
+) -> Dict:
+    query = {}
+    
+    if name:
+        query['name'] = {'$regex': name, '$options': 'i'}
+    if tags:
+        query['tags'] = {'$all': tags.split(',')}
+    if min_price is not None:
+        query['price'] = {'$gte': min_price}
+    if max_price is not None:
+        if 'price' in query:
+            query['price']['$lte'] = max_price
+        else:
+            query['price'] = {'$lte': max_price}
+    
+    return query
+
 # Add filtering using URL Params
 @router.get('/product', response_model=List[Product_w_ID])
-async def get_products(): #query: Annotated[dict, Depends(filtering)]):
+async def get_products(query: Annotated[dict, Depends(filter_params)]):
     """Get All Products"""
     products_list = [
         Product_w_ID(
             id=str(x.get('_id')),
             **x
-        ) for x in products.find() # products.aggregate(q)
+        ) for x in products.find(query)
     ]
     return products_list
 
